077678654e06ab77b9ce605aeeea1ff2525feb7c
Dieter Flick <dflick@pivotal.io>
2017-03-15 08:05:40 +0100

    merge code from master

--- boot-twitter-visualizer/src/main/java/io/pivotal/twitter/visualizer/repositories/TweetDataRepository.java
+++ boot-twitter-visualizer/src/main/java/io/pivotal/twitter/visualizer/repositories/TweetDataRepository.java
@@ -1,14 +1,15 @@
 package io.pivotal.twitter.visualizer.repositories;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import io.pivotal.twitter.model.TweetData;
-
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //import feign.RequestLine;
  
@@ -26,5 +27,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 		@RestResource(path = "sentiment", rel = "sentiment")
 		Page<TweetData> findBySentiment(@Param("q") int sentiment, Pageable pageable);
+		
+		@RestResource(path = "top", rel = "top")
+		List<TweetData> findFirst20ByOrderByIdDesc();
 	}
 	
